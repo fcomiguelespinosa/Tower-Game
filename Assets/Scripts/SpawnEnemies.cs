e3c1d58cbcb5c61e132761c67e70feabf371d9fd
Francisco M Espinosa <fcomiguelespinosa@gmail.com>
2019-10-18 23:37:19 -0500

    Generate Enemies

---
+++ Assets/Scripts/SpawnEnemies.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemies : MonoBehaviour
+{
+	public GameObject Enemy;
+	private float timerSpawn;
+	private float waitforEnemy; 	
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+    	timerSpawn = 0;
+    	waitforEnemy = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	timerSpawn += Time.deltaTime;
+    	
+    	if(timerSpawn >= waitforEnemy ){
+    		timerSpawn = 0;
+    		
+	        Instantiate (Enemy, new Vector3(Random.Range(-10.0f, 10.0f), 0, Random.Range(-10.0f, 10.0f)), Quaternion.identity);	
+    	}  	
+    }
+}
